40c2b66a89a66f906ad5518f07a0490d410821fe
NH <znirgal@gmail.com>
2021-06-12 16:16:12 +0200

    fix

--- server_ssl.js
+++ server_ssl.js
@@ -11,7 +11,10 @@ httpApp.use(express.static(__dirname + "/static/"));
 
 var port = process.env.PORT || 8080;
 // Start Express https server on port 8443
-var webServer = https.createServer(httpApp);
+var webServer = https.createServer({
+    key:  fs.readFileSync(__dirname + "/certs/localhost.key"),
+    cert: fs.readFileSync(__dirname + "/certs/localhost.crt")
+},httpApp);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
