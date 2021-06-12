4b8f4ab0b7b9c675ba03edfa83d90e023cbd7a6e
NH <znirgal@gmail.com>
2021-06-12 16:57:40 +0200

    any number of webodies

--- static/main.js
+++ static/main.js
@@ -2,20 +2,25 @@
 
 $(document).ready(() => {
     $(document).on('mousemove', function (e) {
-            $('#my-body').css({top: e.clientY, left: e.clientX});
+        $('#my-body').css({top: e.clientY, left: e.clientX});
 
-            easyrtc.sendDataWS({targetRoom: "default"}, "message", JSON.stringify({clientX: e.clientX, clientY: e.clientY}), null);
-        });
+        easyrtc.sendDataWS({targetRoom: "default"}, "message", JSON.stringify({
+            clientX: e.clientX,
+            clientY: e.clientY
+        }), null);
+    });
 
     connect(true);
 
 });
 
 var selfEasyrtcid = "";
+var webodies = {};
+
 function mousePosition(who, msgType, content) {
-    console.log(who + " " + msgType + " " + content);
+    // console.log(who + " " + msgType + " " + content);
     var position = JSON.parse(content);
-    $('#other-body').css({top: position.clientY, left: position.clientX});
+    $('#' + who).css({top: position.clientY, left: position.clientX});
 }
 
 //joins a room
@@ -32,7 +37,7 @@ function connectToRoom(roomName) {
 function connect(immediateMode) {
     connectToRoom("default");
     easyrtc.setPeerListener(mousePosition);
-    // easyrtc.setRoomOccupantListener(convertListToButtons);
+    easyrtc.setRoomOccupantListener(occupantsListener);
 
     var mysocket = io.connect(null, {
         'connect timeout': 10000,
@@ -42,60 +47,50 @@ function connect(immediateMode) {
 
     if (!mysocket) {
         throw "io.connect failed";
-    }
-    else {
+    } else {
         console.log("application allocated socket ", mysocket);
         easyrtc.useThisSocketConnection(mysocket);
     }
-    if( immediateMode) {
+    if (immediateMode) {
         easyrtc.connect("easyrtc.reconnect", loginSuccess, loginFailure);
-    }
-    else {
-        setTimeout(function() {
+    } else {
+        setTimeout(function () {
             easyrtc.connect("easyrtc.reconnect", loginSuccess, loginFailure);
-        }, 10*1000);
+        }, 10 * 1000);
     }
 }
 
-
-function convertListToButtons (roomName, occupants, isPrimary) {
-
-    for(var easyrtcid in occupants) {
-        var button = document.createElement('button');
-        button.onclick = function(easyrtcid) {
-            return function() {
-                sendStuffWS(easyrtcid);
-            };
-        }(easyrtcid);
-        var label = document.createTextNode("Send to " + easyrtc.idToName(easyrtcid));
-        button.appendChild(label);
-
-        otherClientDiv.appendChild(button);
-    }
-    if( !otherClientDiv.hasChildNodes() ) {
-        otherClientDiv.innerHTML = "<em>Nobody else logged in to talk to...</em>";
+function occupantsListener(roomName, occupants, isPrimary) {
+    console.log(occupants)
+    for (const key in occupants) {
+        if (!webodies[key]) {
+            webodies[key] = '';
+            $('#webodies-div').append(
+                `<span class="webody" style="font-size: 48px; color: #${intToRGB(occupants[key].roomJoinTime)}; position: absolute" id="${key}">
+<i class="far fa-smile"></i>
+</span>`);
+        }
     }
 }
-
-
-function sendStuffWS(otherEasyrtcid) {
-    var text = document.getElementById('sendMessageText').value;
-    if(text.replace(/\s/g, "").length === 0) { // Don't send just whitespace
-        return;
-    }
-
-    easyrtc.sendDataWS(otherEasyrtcid, "message",  text);
-    mousePosition("Me", "message", text);
-    document.getElementById('sendMessageText').value = "";
-}
-
 
 function loginSuccess(easyrtcid) {
     selfEasyrtcid = easyrtcid;
     console.log(selfEasyrtcid);
 }
 
-
 function loginFailure(errorCode, message) {
     easyrtc.showError(errorCode, message);
+}
+
+function intToRGB(i) {
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    var result = "00000".substring(0, 6 - c.length) + c;
+    result.replace("F", "D")
+        .replace("E", "C")
+        .replace("0", "2")
+        .replace("1", "3");
+    return result;
 }
